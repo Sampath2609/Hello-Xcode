31828cada39245ba8a6279e2998eb5acfbf5d966
Vagicharla,Venkat Raghava Sampath <s533978@ch140016m.nwmissouri.edu>
2019-01-31 13:17:32 -0600

    Initial Commit

---
+++ Hello Xcode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello Xcode
+//
+//  Created by Vagicharla,Venkat Raghava Sampath on 1/31/19.
+//  Copyright © 2019 Vagicharla,Venkat Raghava Sampath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
